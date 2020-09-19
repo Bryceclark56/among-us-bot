97be3f78318c0bd945c5ab8291f5f53762e8f13c
Bryce Clark <bryce@bc56.me>
2020-09-18 21:47:17 -0500

    Mute functionality in voice channel
    - Only works in voice channels named "Among Us"

--- bot.js
+++ bot.js
@@ -1,13 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const command_prefix = "??";
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (!msg.content.startsWith(command_prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(command_prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
         msg.reply('pong');
+    }
+    else if (command === "mute") {
+        msg.reply("Muting all imposters");
+        msg.guild.channels.cache
+            .filter(channel => channel.type === "voice" && channel.name === "Among Us")
+            .first()
+            .members
+            .forEach(member => {
+                if (!member.voice.mute) {
+                    member.voice.setMute(true, "Among Us");
+                }
+            })
+    }
+    else if (command === "unmute") {
+        msg.reply("Unmuting crewmembers");
+        msg.guild.channels.cache
+            .filter(channel => channel.type === "voice" && channel.name === "Among Us")
+            .first()
+            .members
+            .forEach(member => {
+                if (member.voice.mute) {
+                    member.voice.setMute(false, "Among Us");
+                }
+            })
     }
 });
 

f972a14807cef76587cb9e1d9f0bbc729163f47e
Bryce Clark <bryce@bc56.me>
2020-09-18 22:11:12 -0500

    Mute toggle command
    - Convenience functions added for code reuse

--- bot.js
+++ bot.js
@@ -1,44 +1,63 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const command_prefix = "??";
+const commandPrefix = "??";
+
+function getAmongUsChannel(guild) {
+    guild.channels.cache
+        .filter(channel => channel.type === "voice" && channel.name === "Among Us")
+        .first()
+
+}
+
+function muteAllInChannel(channel, mute, reason) {
+    channel.members
+        .forEach(member => {
+            if (member.voice.mute !== mute) {
+                member.voice.setMute(mute, reason);
+            }
+        })
+}
+
+let amongUsMuted = false;
+function toggleAmongUsMute(guild) {
+    const channel = getAmongUsChannel(guild);
+
+    amongUsMuted = !amongUsMuted;
+    muteAllInChannel(channel, amongUsMuted, "Among Us");
+}
+
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(command_prefix) || msg.author.bot) return;
+    if (!msg.content.startsWith(commandPrefix) || msg.author.bot) return;
 
-    const args = msg.content.slice(command_prefix.length).trim().split(/ +/);
+    const args = msg.content.slice(commandPrefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'ping') {
-        msg.reply('pong');
-    }
-    else if (command === "mute") {
-        msg.reply("Muting all imposters");
-        msg.guild.channels.cache
-            .filter(channel => channel.type === "voice" && channel.name === "Among Us")
-            .first()
-            .members
-            .forEach(member => {
-                if (!member.voice.mute) {
-                    member.voice.setMute(true, "Among Us");
-                }
-            })
-    }
-    else if (command === "unmute") {
-        msg.reply("Unmuting crewmembers");
-        msg.guild.channels.cache
-            .filter(channel => channel.type === "voice" && channel.name === "Among Us")
-            .first()
-            .members
-            .forEach(member => {
-                if (member.voice.mute) {
-                    member.voice.setMute(false, "Among Us");
-                }
-            })
+    switch(command) {
+        case "ping": {
+            msg.reply("pong");
+        } break;
+
+        case "mute": {
+            msg.reply("Muting all imposters");
+            muteAllInChannel(getAmongUsChannel(msg.guild), true, "Among Us");
+        } break;
+
+        case "unmute": {
+            msg.reply("Unmuting crewmembers");
+            muteAllInChannel(getAmongUsChannel(msg.guild), false, "Among Us");
+        } break;
+
+        case "toggle": {
+            msg.reply("Toggling Among Us channel muted state")
+            toggleAmongUsMute(msg.guild);
+        } break;
     }
 });
 
